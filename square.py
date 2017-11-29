59f2008d04542a289e86d18518cb0502221cc460
Thomas Kluyver <thomas@kluyver.me.uk>
2017-11-29 18:31:51 +0000

    Check that we get an integer

--- square.py
+++ square.py
@@ -3,5 +3,9 @@ if len(sys.argv) < 2:
     print('Give me a number!')
     sys.exit(1)
 
+if not (sys.argv[1].isdigit() or (sys.argv[1].startswith('-') and sys.argv[1][1:].isdigit())):
+    print(sys.argv[1], 'is not an integer')
+    sys.exit(1)
+
 n = int(sys.argv[1])
 print(n * n)
