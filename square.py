df77541c12e3ddd73b178e8b5ccb42c5fe0abfe9
Thomas Kluyver <thomas@kluyver.me.uk>
2017-11-29 18:35:21 +0000

    Better check for integers

--- square.py
+++ square.py
@@ -3,11 +3,12 @@ if len(sys.argv) < 2:
     print('Give me a number!')
     sys.exit(1)
 
-if not (sys.argv[1].isdigit() or (sys.argv[1].startswith('-') and sys.argv[1][1:].isdigit())):
+try:
+    n = int(sys.argv[1])
+except ValueError:
     print(sys.argv[1], 'is not an integer')
     sys.exit(1)
 
-n = int(sys.argv[1])
 ref = 0
 for i in range(n):
     ref += n
