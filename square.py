9e8964cc7ff160080e3c8f2dc20243b1f911cbfe
Thomas Kluyver <thomas@kluyver.me.uk>
2017-11-29 18:44:23 +0000

    Make it a function

--- square.py
+++ square.py
@@ -1,15 +1,20 @@
 import sys
-if len(sys.argv) < 2:
-    print('Give me a number!')
-    sys.exit(1)
 
-try:
-    n = int(sys.argv[1])
-except ValueError:
-    print(sys.argv[1], 'is not an integer')
-    sys.exit(1)
+def main():
+    if len(sys.argv) < 2:
+        print('Give me a number!')
+        sys.exit(1)
 
-ref = 0
-for i in range(n):
-    ref += n
-print(ref)
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print(sys.argv[1], 'is not an integer')
+        sys.exit(1)
+
+    ref = 0
+    for i in range(n):
+        ref += n
+    print(ref)
+
+if __name__ == '__main__':
+    main()
